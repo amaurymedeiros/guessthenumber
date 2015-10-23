51c0d3088a7afb8eddfd114d70d9b381bd9c2586
Amaury Medeiros <amaurymedeiros@gmail.com>
2015-10-23 17:55:36 -0300

    Improve game ending

--- guessthenumber/static/guess.js
+++ guessthenumber/static/guess.js
@@ -3,7 +3,7 @@ function guessNumber(encrypted) {
   $.post($SCRIPT_ROOT + '/guess_number',
     {'encrypted' : encrypted, 'guess' : guess},
     function(data) {
-      updateResult(guess, data);
+      checkResult(guess, data);
     });
 }
 
@@ -11,4 +11,22 @@ function updateResult(guess, data) {
   var parsedData = data['white_pins'] + ' white pin(s). ' + data['black_pins']
                     + ' black pin(s).';
   $('#result').append('<p>' + guess + ': ' + parsedData + '</p>');
+}
+
+function checkResult(guess, data) {
+  if (data['black_pins'] == 4) {
+    endGame(guess);
+    return;
+  }
+  updateResult(guess, data);
+}
+
+function endGame(guess) {
+  // Fix numbers starting with 0
+  if (guess.length == 3) {
+    guess = '0' + guess;
+  }
+  $('#game').html('<p> Congratulations! You guessed the number: ' + guess 
+                  + '</p>');
+  $('#try_again').show();
 }

b5643d6103377158c10e7965d082f3a910be2a51
Amaury Medeiros <amaurymedeiros@gmail.com>
2015-10-23 17:18:40 -0300

    Simplify get secret logic

--- guessthenumber/views.py
+++ guessthenumber/views.py
@@ -3,13 +3,9 @@ from flask import jsonify, render_template, request
 from . import app, xor
 from utils import calculate_score, get_random_number
 
-secret_number = 0
-guesses = []
-
 @app.route("/")
 def home():
-    _set_secret_number()
-    return render_template('index.html', secret=secret_number)
+    return render_template('index.html', secret=get_random_number())
 
 @app.route('/guess_number', methods=['POST'])
 def guess_number():
@@ -17,6 +13,3 @@ def guess_number():
     guess = int(request.form.get('guess'))
     return jsonify(calculate_score(secret, guess))
 
-def _set_secret_number():
-    global secret_number
-    secret_number = get_random_number()

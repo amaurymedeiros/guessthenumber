7817c4f8a40d406c7f3150e814fd2f61ba6dd0ae
Amaury Medeiros <amaurymedeiros@gmail.com>
2015-10-23 17:07:25 -0300

    Add simple guessing logic

--- guessthenumber/static/guess.js
+++ guessthenumber/static/guess.js
@@ -3,6 +3,12 @@ function guessNumber(encrypted) {
   $.post($SCRIPT_ROOT + '/guess_number',
     {'encrypted' : encrypted, 'guess' : guess},
     function(data) {
-      console.log(data);
+      updateResult(guess, data);
     });
+}
+
+function updateResult(guess, data) {
+  var parsedData = data['white_pins'] + ' white pin(s). ' + data['black_pins']
+                    + ' black pin(s).';
+  $('#result').append('<p>' + guess + ': ' + parsedData + '</p>');
 }
